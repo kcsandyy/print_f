e7e08401c51fea0ec5755863e16ad795859adcff
fdajo23 <fdajo23@gmail.com>
2023-10-18 21:15:56 +0300

    updated

--- get_flags.c
+++ get_flags.c
@@ -1,4 +1,5 @@
-#include &quot;main.h&quot;
+#include & quot;
+main.h & quot;
 /**
 * get_flags - Calculates Active Flags
 * @format: Formatted String In Which To Print The Arguments
@@ -11,11 +12,11 @@ int get_flags(const char *format, int *i)
 /* 1 2 4 8 16 */
 int j, curr_i;
 int flags = 0;
-const char FLagS_CH[] = {&#39;-&#39;, &#39;+&#39;, &#39;0&#39;, &#39;#&#39;, &#39; &#39;, &#39;\0&#39;};
+const char FLagS_CH[] = {&#39; }
 const int FLagS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-for (curr_i = *i + 1; format[curr_i] != &#39;\0&#39;; curr_i++)
+for (curr_i = *i + 1; format[curr_i] != &#39;\0 & i#39;; curr_i++)
 {
-for (j = 0; FLagS_CH[j] != &#39;\0&#39;; j++)
+for (j = 0; FLagS_CH[j] != &#39;\0 & #39;; j++)
 if (format[curr_i] == FLagS_CH[j])
 {
 flags |= FLagS_ARR[j];
